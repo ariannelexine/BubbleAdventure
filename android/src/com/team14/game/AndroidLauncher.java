4414c251c3403ad18c6869e53c2985ea6c20c8b2
Arianne Silvestre <ariannesilvestre23@yahoo.com>
2017-03-26 21:21:48 -0500

    Init

---
+++ android/src/com/team14/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.team14.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.team14.game.BubbleAdventure;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BubbleAdventure(), config);
+	}
+}
