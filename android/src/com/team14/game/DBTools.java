cc609172b8e9c54f68927fde042b110cf508ff54
graciela aguilar <gracielaaguilar@gracielas-MBP.attlocal.net>
2017-04-16 02:06:03 -0500

    login and database implemented, revisions to be made

---
+++ android/src/com/team14/game/DBTools.java
@@ -0,0 +1,90 @@
+package com.team14.game;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by gracielaaguilar on 4/15/17.
+ */
+
+
+
+public class DBTools extends SQLiteOpenHelper {
+
+    private final static int    DB_VERSION = 10;
+    public static final String TABLE_AD_CONFIGURATIONS = "adConfigurations";
+
+    public DBTools(Context context) {
+        super(context, "myApp.db", null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "create table logins (userId Integer primary key autoincrement, "+
+                " username text, password text)";
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        try{
+            System.out.println("UPGRADE DB oldVersion="+oldVersion+" - newVersion="+newVersion);
+            recreatedb(sqLiteDatabase);
+            if (oldVersion<10){
+                String query = "create table logins (userId Integer primary key autoincrement, "+
+                        " username text, password text)";
+                sqLiteDatabase.execSQL(query);
+            }
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+
+    private void recreatedb(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_AD_CONFIGURATIONS);
+        onCreate(sqLiteDatabase);
+    }
+
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // super.onDowngrade(db, oldVersion, newVersion);
+        System.out.println("DOWNGRADE DB oldVersion="+oldVersion+" - newVersion="+newVersion);
+    }
+
+    public User insertUser (User queryValues){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("username", queryValues.username);
+        values.put("password", queryValues.password);
+        queryValues.userId=database.insert("logins", null, values);
+        database.close();
+        return queryValues;
+    }
+
+    public int updateUserPassword (User queryValues){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("username", queryValues.username);
+        values.put("password", queryValues.password);
+        queryValues.userId=database.insert("logins", null, values);
+        database.close();
+        return database.update("logins", values, "userId = ?", new String[] {String.valueOf(queryValues.userId)});
+    }
+
+    public User getUser (String username){
+        String query = "Select userId, password from logins where username ='"+username+"'";
+        User myUser = new User(0,username,"");
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery(query, null);
+        if (cursor.moveToFirst()){
+            do {
+                myUser.userId=cursor.getLong(0);
+                myUser.password=cursor.getString(1);
+            } while (cursor.moveToNext());
+        }
+        return myUser;
+    }
+}
---
+++ android/src/com/team14/game/User.java
@@ -0,0 +1,19 @@
+package com.team14.game;
+
+
+/**
+ * Created by gracielaaguilar on 4/15/17.
+ */
+
+public class User {
+    public long userId;
+    public String username;
+    public String password;
+
+    public User(long userId, String username, String password){
+        this.userId=userId;
+        this.username=username;
+        this.password=password;
+    }
+
+}
