44e8262f79e8363492daefd5e6bb843040a9e777
Anil <parshad.anil@gmail.com>
2017-04-21 22:04:35 -0500

    fixed

--- android/src/com/team14/game/AndroidLauncher.java
+++ android/src/com/team14/game/AndroidLauncher.java
@@ -9,17 +9,12 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 
 public class AndroidLauncher extends AndroidApplication {
-<<<<<<< HEAD
-	SharedPreferences pref;
-=======
-
->>>>>>> 68f0fd0f5e16f9f62fa0c2c0c16b27012b3c9e92
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		pref = getSharedPreferences("com.team14.game", Context.MODE_PRIVATE);
+		SharedPreferences pref = getSharedPreferences("com.team14.game", Context.MODE_PRIVATE);
 		if(!(pref.contains("highscore")))
 		{
 			pref.edit().putInt("highscore", 0);
