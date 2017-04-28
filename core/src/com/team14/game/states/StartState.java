8af0443ad8544a1d1add49292c8fc1b0be290071
William Caballero <wcaballero4@gmail.com>
2017-04-27 21:36:36 -0500

    added description under tap

--- core/src/com/team14/game/states/StartState.java
+++ core/src/com/team14/game/states/StartState.java
@@ -1,9 +1,12 @@
 package com.team14.game.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.team14.game.BubbleAdventure;
+
 
 /**
  * Created by Arianne on 3/31/17.
@@ -13,12 +16,18 @@ import com.team14.game.BubbleAdventure;
 public class StartState extends State {
 
     private Texture startBubble, bg, tap;
+    private BitmapFont description;
+    private String descString;
 
     public StartState(GameStateManager gsm) {
         super(gsm);
         cam.setToOrtho(false, BubbleAdventure.WIDTH / 2, BubbleAdventure.HEIGHT / 2);
         bg = new Texture("bg1.jpg");
         tap = new Texture("Tap.png");
+        descString = "BUBBA "+ " EATS VEGETABLES TO \nACCUMULATE POINTS. EATING \nJUNK FOOD WILL MAKE HIM \nFLUFFY AND " +
+                "TO MUCH FOOD \nWILL MAKE HIM POP. OH AND \nWATCH OUT FOR OBSTACLES!\n";
+        description = new BitmapFont();
+        description.getData().setScale(0.8f, 0.8f);//resize text by scaling it
         startBubble = new Texture("SmallBubble.png");
 
     }
@@ -42,6 +51,8 @@ public class StartState extends State {
         sb.begin();
         sb.draw(bg, 0,0);
         sb.draw(tap, 50, 200); //should probably not hard code will deal wid it l8r
+        description.setColor(Color.CYAN);
+        description.draw(sb, descString, 30, 130);
         sb.draw(startBubble, 50, 150);
         sb.end();
     }
@@ -51,5 +62,6 @@ public class StartState extends State {
         startBubble.dispose();
         bg.dispose();
         tap.dispose();
+        description.dispose();
     }
 }
